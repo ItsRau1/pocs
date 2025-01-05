4577369165956cd6a8a1637bf19f723fd1454fef
Ruan Dias <ruan.dias@muralis.com.br>
2025-01-05 17:24:50 -0300

    feat: alter "MakeCheckIn" & "ChecoutTest" -> "CheckoutTest"

--- back/spring/src/main/java/com/poc/core/usecase/MakeCheckIn.java
+++ back/spring/src/main/java/com/poc/core/usecase/MakeCheckIn.java
@@ -1,7 +1,7 @@
 package com.poc.core.usecase;
 
 import com.poc.core.domain.dto.MakeCheckInDTO;
-import com.poc.core.domain.entity.CheckIn;
+import com.poc.core.domain.entity.Check;
 import com.poc.core.domain.repository.CheckRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,14 +12,14 @@ public class MakeCheckIn {
 
 	private final CheckRepository checkRepository;
 
-	public CheckIn execute(MakeCheckInDTO dto) {
-		CheckIn checkIn = CheckIn.builder()
+	public Check execute(MakeCheckInDTO dto) {
+		Check check = Check.builder()
 			.plate(dto.getPlate())
 			.location(dto.getLocation())
 			.checkInDate(dto.getCheckInDate())
 			.build();
-		checkIn.register();
-		return checkRepository.save(checkIn);
+		check.register();
+		return checkRepository.save(check);
 	}
 
 }
--- back/spring/src/test/java/com/poc/core/integration/CheckoutTest.java
+++ back/spring/src/test/java/com/poc/core/integration/CheckoutTest.java
@@ -1,11 +1,10 @@
 package com.poc.core.integration;
 
 import com.poc.core.domain.dto.MakeCheckInDTO;
-import com.poc.core.domain.entity.CheckIn;
+import com.poc.core.domain.entity.Check;
 import com.poc.core.usecase.MakeCheckIn;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +14,7 @@ import java.util.UUID;
 
 @SpringBootTest
 @RequiredArgsConstructor
-public class ChecoutTest {
+public class CheckoutTest {
 
 	@Autowired
 	MakeCheckIn makeCheckIn;
@@ -30,7 +29,7 @@ public class ChecoutTest {
 			.checkInDate(LocalDateTime.now())
 			.plate(UUID.randomUUID().toString().substring(0, 9))
 			.build();
-		CheckIn checkin = makeCheckIn.execute(makeCheckInDTO);
+		Check checkin = makeCheckIn.execute(makeCheckInDTO);
 		Assertions.assertNotNull(checkin);
 		Assertions.assertEquals(makeCheckInDTO.getPlate(), checkin.getPlate());
 		Assertions.assertEquals(makeCheckInDTO.getCheckInDate(), checkin.getCheckInDate());
